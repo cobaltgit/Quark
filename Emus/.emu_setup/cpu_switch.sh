fdf834d6cbef5bb885e81c9e234b9ac4b48e713a
cobaltgit <cobaltmail4204@gmail.com>
2025-03-11 13:19:10 +0000

    Shorten X script names

--- Emus/.emu_setup/cpu_switch.sh
+++ Emus/.emu_setup/cpu_switch.sh
@@ -9,19 +9,19 @@ OPT="/mnt/SDCARD/Emus/.emu_setup/opts/${EMU}.opt"
 case "$CPU_MODE" in
     "smart")
         NEW_MODE="performance"
-        NEW_DISPLAY="Smart-(✓PERFORMANCE)-Overclock"
+        NEW_DISPLAY="CPU: Perf"
         ;;
     "performance")
         NEW_MODE="overclock"
-        NEW_DISPLAY="Smart-Performance-(✓OVERCLOCK)"
+        NEW_DISPLAY="CPU: OC"
         ;;
     "overclock")
         NEW_MODE="smart"
-		NEW_DISPLAY="(✓SMART)-Performance-Overclock"
+		NEW_DISPLAY="CPU: Smart"
         ;;
 esac
 
-sed -i "s|\"CPU Mode:.*\"|\"CPU Mode: $NEW_DISPLAY\"|g" "$CONFIG"
+sed -i "s|\"CPU:.*\"|\"CPU: $NEW_DISPLAY\"|g" "$CONFIG"
 sed -i "s|CPU_MODE=.*|CPU_MODE=\"$NEW_MODE\"|g" "$OPT"
 
 sleep 1
