22162ee5c5610fd76d2128cfa26472e972303e94
cobaltgit <cobaltmail4204@gmail.com>
2025-03-11 13:11:32 +0000

    Increase smart mode CPU ceiling

--- Emus/.emu_setup/launch_emu.sh
+++ Emus/.emu_setup/launch_emu.sh
@@ -24,11 +24,11 @@ set_cpuclock() {
             echo 1 >/sys/devices/system/cpu/cpufreq/conservative/sampling_down_factor
             echo 400000 >/sys/devices/system/cpu/cpufreq/conservative/sampling_rate
             echo "$CPU_MIN_FREQ" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-            echo 1200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+            echo 1344000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
             ;;
         "performance")
             echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-            echo 1440000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+            echo 1344000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
             ;;
         "overclock")
             echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
