0f9ced7f38a8b8ea07cc7d61a5764888d873ec58
cobaltgit <cobaltmail4204@gmail.com>
2025-03-15 21:14:17 +0000

    Emuclean: remove leading spaces!

--- Apps/EmuClean/launch.sh
+++ Apps/EmuClean/launch.sh
@@ -6,8 +6,8 @@ EMU_DIR="/mnt/SDCARD/Emus"
 
 for EMU in "$EMU_DIR"/*; do
     if [ -d "$EMU" ]; then
-        EXTENSIONS="$(awk -F ':' '/extlist/ {print $2}' "$EMU/config.json" | sed 's/[",]//g')" # no jq? no problem. might add it later down the line though
-        ROM_DIR="$EMU/$(awk -F ':' '/rompath/ {print $2}' "$EMU/config.json" | sed 's/[",]//g')"
+        EXTENSIONS="$(awk -F ':' '/extlist/ {print $2}' "$EMU/config.json" | sed 's/^[[:space:]]*//; s/[",]//g')" # no jq? no problem. might add it later down the line though
+        ROM_DIR="$EMU/$(awk -F ':' '/rompath/ {print $2}' "$EMU/config.json" | sed 's/^[[:space:]]*//; s/[",]//g')"
         ROM_COUNT="$(find "$ROM_DIR" -type f ! -path "*/Imgs/*" ! -name *.xml ! -name *.txt ! -name ".gitkeep" ! -name "*cache7.db" | sed '/^\s*$/d' | grep -icE "\.($EXTENSIONS)$")"
         
         if [ "$ROM_COUNT" -eq 0 ]; then
