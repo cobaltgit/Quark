b1b232540dc8bcc754d8b2e7137ee6c444a5b966
cobaltgit <cobaltmail4204@gmail.com>
2025-03-16 10:38:31 +0000

    Fix error resulting in all calls to set_cpuclock setting to smart mode

--- System/bin/helpers.sh
+++ System/bin/helpers.sh
@@ -7,7 +7,7 @@ set_cpuclock() {
     chmod a+w /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
     case $1 in
-        "smart"|*)
+        "smart")
             echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
             echo 50 >/sys/devices/system/cpu/cpufreq/conservative/down_threshold
             echo 80 >/sys/devices/system/cpu/cpufreq/conservative/up_threshold
