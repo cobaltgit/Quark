b7c09b68ee748436f55567498934a2d13735d884
cobaltgit <cobaltmail4204@gmail.com>
2025-03-19 12:31:28 +0000

    First shot at an archive unpacker on boot

---
+++ System/starts/1-archive-unpacker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ARCHIVES_FOLDER="/mnt/SDCARD/System/archives"
+ARCHIVE_UNPACK_LOG="/mnt/SDCARD/System/log/archive_unpack.log"
+
+find "$ARCHIVES_FOLDER" -type f -iname "*.zip" | while read archive; do
+    basename="$(basename "$archive")"
+    echo "Unpacking archive $basename"
+    if ! unzip -o -d /mnt/SDCARD "$archive"; then
+        echo "Failed to unpack archive $basename"
+    else
+        echo "Archive $basename unpacked successfully"
+        rm "$archive"
+    fi
+done > "$ARCHIVE_UNPACK_LOG" 2>&1
