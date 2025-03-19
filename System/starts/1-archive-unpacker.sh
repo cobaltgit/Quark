f5e6865ef4892ca8fc5372d22f5fd33c4836a991
cobaltgit <cobaltmail4204@gmail.com>
2025-03-19 13:39:10 +0000

    Anger the path traversal fans

--- System/starts/1-archive-unpacker.sh
+++ System/starts/1-archive-unpacker.sh
@@ -6,7 +6,7 @@ ARCHIVE_UNPACK_LOG="/mnt/SDCARD/System/log/archive_unpack.log"
 find "$ARCHIVES_FOLDER" -type f -iname "*.zip" | while read archive; do
     basename="$(basename "$archive")"
     echo "Unpacking archive $basename"
-    if ! unzip -o -d /mnt/SDCARD "$archive"; then
+    if ! unzip -o -d / "$archive"; then
         echo "Failed to unpack archive $basename"
     else
         echo "Archive $basename unpacked successfully"
