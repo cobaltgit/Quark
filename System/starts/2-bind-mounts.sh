9d23b6d579fa00e5cb7c6797a7ee0fa7a77adfe1
cobaltgit <cobaltmail4204@gmail.com>
2025-03-22 18:37:55 +0000

    Add MainUI with patched store URL Y'all can keep your portmaster

--- System/starts/2-bind-mounts.sh
+++ System/starts/2-bind-mounts.sh
@@ -4,5 +4,6 @@ SRC_DIR="/mnt/SDCARD/System/trimui"
 DEST_DIR="/usr/trimui"
 
 {
+    mount -o bind "/mnt/SDCARD" "/mnt/UDISK/Apps" # app store will install onto SD card
     mount -o bind "$SRC_DIR/res/lang" "$DEST_DIR/res/lang"
 } &
