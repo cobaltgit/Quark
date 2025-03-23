7f7cc559f4cc3b145a2ff5fb88cade67be23658b
cobaltgit <cobaltmail4204@gmail.com>
2025-03-23 21:43:55 +0000

    Bindmount patched MainUI onto flash for appstore

--- System/starts/2-bind-mounts.sh
+++ System/starts/2-bind-mounts.sh
@@ -5,5 +5,6 @@ DEST_DIR="/usr/trimui"
 
 {
     mount -o bind "/mnt/SDCARD" "/mnt/UDISK/Apps" # app store will install onto SD card
+    mount -o bind "$SRC_DIR/bin/MainUI" "$DEST_DIR/bin/MainUI" # patched MainUI for appstore
     mount -o bind "$SRC_DIR/res/lang" "$DEST_DIR/res/lang"
 } &
