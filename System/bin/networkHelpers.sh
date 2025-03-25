38185f3390e9a9e754f5b746686dce1926055ea5
cobaltgit <cobaltmail4204@gmail.com>
2025-03-25 18:33:22 +0000

    Redirect pgrep to null

--- System/bin/networkHelpers.sh
+++ System/bin/networkHelpers.sh
@@ -3,13 +3,13 @@
 DUFS_LOGFILE="/mnt/SDCARD/System/log/dufs.log"
 
 start_dufs_process() {
-    if ! pgrep dufs; then
+    if ! pgrep dufs >/dev/null 2>&1; then
         /mnt/SDCARD/System/bin/dufs --allow-upload --allow-delete --allow-search --allow-archive --log-file "$DUFS_LOGFILE" "/mnt/SDCARD" &
     fi
 }
 
 stop_dufs_process() {
-    if pgrep dufs; then
+    if pgrep dufs >/dev/null 2>&1; then
         killall -9 dufs
     fi
 }
