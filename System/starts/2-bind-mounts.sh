5162a5c9e2a02a4dd86f8f576d8f9fc00ba33412
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-04 11:42:42 +0100

    Don't remove cache.zip as that already gets removed

--- System/starts/2-bind-mounts.sh
+++ System/starts/2-bind-mounts.sh
@@ -4,7 +4,6 @@ SRC_DIR="/mnt/SDCARD/System/trimui"
 DEST_DIR="/usr/trimui"
 
 {
-    rm -f /mnt/UDISK/Store/cache.zip
     rm -rf /mnt/UDISK/Store/.cache
 
     mount -o bind "/mnt/SDCARD/System/scripts/usb_storage_disabled.sh" "/usr/trimui/apps/usb_storage/launch.sh" # disable USB storage app
