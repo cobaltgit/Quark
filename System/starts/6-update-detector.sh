6da14cec69cc8aef0e1971d28f7dbc4a112cb879
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-05 18:29:55 +0100

    Update detector: pick up tar.zst packages instead of zip

--- System/starts/6-update-detector.sh
+++ System/starts/6-update-detector.sh
@@ -2,7 +2,7 @@
 # Detects update packages and makes the updater app visible
 
 UPDATER_APP_CONFIG="/mnt/SDCARD/Apps/QuarkUpdater/config.json"
-UPDATE_PKG="$(ls -t /mnt/SDCARD/Quark_Update_*.zip | head -1)"
+UPDATE_PKG="$(ls -t /mnt/SDCARD/Quark_Update_*.tar.zst | head -1)"
 
 if [ -f "$UPDATE_PKG" ]; then
     sed -i 's/^{{*$/{/' "$UPDATER_APP_CONFIG" # make visible
