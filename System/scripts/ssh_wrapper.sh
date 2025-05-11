d7a68ace25f359c0b8e53dfc55532a1bb042db24
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-11 15:09:02 +0100

    Refactor ssh wrapper

--- System/scripts/ssh_wrapper.sh
+++ System/scripts/ssh_wrapper.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
+. /mnt/SDCARD/System/scripts/helpers.sh
+
 cd /mnt/SDCARD
 
-if [ "$SSH_ORIGINAL_COMMAND" = "/usr/libexec/sftp-server" ]; then
-    [ -x "/mnt/SDCARD/System/bin/gesftpserver" ] && exec /mnt/SDCARD/System/bin/gesftpserver
-elif [ -n "$SSH_ORIGINAL_COMMAND" ]; then
-	exec $SSH_ORIGINAL_COMMAND
-else
-	exec /bin/sh
-fi
+case "$SSH_ORIGINAL_COMMAND" in
+	"/usr/libexec/gesftpserver") exec gesftpserver ;;
+	*) exec "${SSH_ORIGINAL_COMMAND:-/bin/sh}"
+esac
