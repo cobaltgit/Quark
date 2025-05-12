f59a896973394d744f031a00fa6719faa366818f
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-12 20:03:36 +0100

    RA app: use 64 bit binary on 64bit platforms

--- Apps/RetroArch/launch.sh
+++ Apps/RetroArch/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+. /mnt/SDCARD/System/scripts/helpers.sh
+
+[ "$PLATFORM" = "tg2040" ] && RA_BIN="ra32.trimui" || RA_BIN="ra64.trimui"
+
 RA_DIR=/mnt/SDCARD/RetroArch
 cd $RA_DIR/
 
-HOME=$RA_DIR/ $RA_DIR/ra32.trimui -v
+HOME=$RA_DIR/ $RA_DIR/$RA_BIN -v
