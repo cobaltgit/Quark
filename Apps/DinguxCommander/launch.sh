d9c2fa45cdcd8df576ac436f12b5d91316920877
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-12 21:21:21 +0100

    all: get Dingux Commander working

--- Apps/DinguxCommander/launch.sh
+++ Apps/DinguxCommander/launch.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+. /mnt/SDCARD/System/scripts/helpers.sh
+
 cd "$(dirname "$0")"
 
 export HOME="/mnt/SDCARD"
 
-./DinguxCommander
+if [ "$PLATFORM" != "tg2040" ]; then
+    gptokeyb "DinguxCommander.$PLATFORM" -c "DinguxCommander.gptk" &
+fi
+
+"./DinguxCommander.$PLATFORM"
 sync
+killall -9 gptokeyb
