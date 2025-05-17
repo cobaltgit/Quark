f3c1758f8ecd6f38b7150f7b9e3e9981bbcc2328
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-17 15:48:23 +0100

    all: unpacker: add zstd support

--- System/starts/1-archive-unpacker.sh
+++ System/starts/1-archive-unpacker.sh
@@ -3,7 +3,7 @@
 . /mnt/SDCARD/System/scripts/helpers.sh
 
 ARCHIVES_FOLDER="/mnt/SDCARD/System/archives"
-ARCHIVE_COUNT="$(ls -1 $ARCHIVES_FOLDER/*.zip | wc -l)"
+ARCHIVE_COUNT="$(ls -1 $ARCHIVES_FOLDER/*.zip $ARCHIVES_FOLDER/*.tar.zst | wc -l)"
 ARCHIVE_UNPACK_LOG="/mnt/SDCARD/System/log/archive_unpack.log"
 
 log_message "Unpacker: $ARCHIVE_COUNT archives found" "$ARCHIVE_UNPACK_LOG"
@@ -13,10 +13,16 @@ if [ "$ARCHIVE_COUNT" -eq 0 ]; then
     exit 0
 fi
 
-find "$ARCHIVES_FOLDER" -type f -iname "*.zip" | while read archive; do
+find "$ARCHIVES_FOLDER" -type f -iname "*.zip" -o -iname "*.tar.zst" | while read archive; do
     basename="$(basename "$archive")"
     log_message "Unpacker: unpacking archive $basename"
-    if ! unzip -o -d / "$archive"; then
+
+    case "$archive" in
+        "*.zip") DECOMPRESS_COMMAND="unzip -o -d / \"$archive\"" ;;
+        "*.tar.zst") DECOMPRESS_COMMAND="zstd -d --stdout \"$archive\" | tar xv -C /" ;;
+    esac
+
+    if ! eval "$DECOMPRESS_COMMAND"; then
         log_message "Unpacker: failed to unpack archive $basename"
     else
         log_message "Unpacker: archive $basename unpacked successfully"
