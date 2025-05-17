b975997d28c23f520c9b8e5f7eb04dab4ea28517
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-17 15:51:48 +0100

    all: unpacker: might as well add tar.gz support

--- System/starts/1-archive-unpacker.sh
+++ System/starts/1-archive-unpacker.sh
@@ -3,7 +3,7 @@
 . /mnt/SDCARD/System/scripts/helpers.sh
 
 ARCHIVES_FOLDER="/mnt/SDCARD/System/archives"
-ARCHIVE_COUNT="$(ls -1 $ARCHIVES_FOLDER/*.zip $ARCHIVES_FOLDER/*.tar.zst | wc -l)"
+ARCHIVE_COUNT="$(ls -1 $ARCHIVES_FOLDER/*.zip $ARCHIVES_FOLDER/*.tar.zst $ARCHIVES_FOLDER/*.tar.gz | wc -l)"
 ARCHIVE_UNPACK_LOG="/mnt/SDCARD/System/log/archive_unpack.log"
 
 log_message "Unpacker: $ARCHIVE_COUNT archives found" "$ARCHIVE_UNPACK_LOG"
@@ -17,8 +17,11 @@ find "$ARCHIVES_FOLDER" -type f -iname "*.zip" -o -iname "*.tar.zst" | while rea
     basename="$(basename "$archive")"
     log_message "Unpacker: unpacking archive $basename"
 
+    display -t "Unpacking archive $basename..."
+
     case "$archive" in
         "*.zip") DECOMPRESS_COMMAND="unzip -o -d / \"$archive\"" ;;
+        "*.tar.gz") DECOMPRESS_COMMAND="tar xzvf \"$archive\" -C /" ;;
         "*.tar.zst") DECOMPRESS_COMMAND="zstd -d --stdout \"$archive\" | tar xv -C /" ;;
     esac
 
