fbffc6701b076d27bb709ee6f49dac7da88a9d58
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-22 09:24:30 +0100

    Revert "hotkeys: use evtest read loop instead of evtest query"
    This reverts commit 1b999bbd119fd0c75628fa130ee130e013ea3fb5.

--- System/starts/7-evtest-hotkeys.sh
+++ System/starts/7-evtest-hotkeys.sh
@@ -3,74 +3,62 @@
 
 . /mnt/SDCARD/System/scripts/helpers.sh
 
-hotkey_listener() {
+reboot_hotkey() {
     BOTH_PRESSED_TIME=0
     MAX_PRESS_LENGTH=10
-
-    SELECT_PRESSED=0
-    START_PRESSED=0
-    MENU_R_PRESSED=0
-
     PROCESSES="MainUI ra32.trimui ra32.trimui_sdl display.elf mp3player.elf OpenBOR.trimui drastic"
 
-    evtest /dev/input/event0 | while read line; do
-        case "$line" in
-            *"EV_KEY"*"KEY_RIGHTCTRL"*"value 1")  # Key 97 pressed
-                SELECT_PRESSED=1
-                ;;
-            *"EV_KEY"*"KEY_RIGHTCTRL"*"value 0")  # Key 97 released
-                SELECT_PRESSED=0
-                BOTH_PRESSED_TIME=0
-                ;;
-            *"EV_KEY"*"KEY_ENTER"*"value 1")      # Key 28 pressed
-                START_PRESSED=1
-                ;;
-            *"EV_KEY"*"KEY_ENTER"*"value 0")      # Key 28 released
-                START_PRESSED=0
-                BOTH_PRESSED_TIME=0
-                ;;
-            *"EV_KEY"*"KEY_PAGEDOWN"*"value 1")   # Key 109 pressed
-                MENU_R_PRESSED=1
-                ;;
-            *"EV_KEY"*"KEY_PAGEDOWN"*"value 0")   # Key 109 released
-                MENU_R_PRESSED=0
-                log_message "MENU+R released" "/mnt/SDCARD/hotkey.log"
-                ;;
-        esac
+    while true; do
+        evtest --query /dev/input/event0 EV_KEY 97
+        if [ $? = 10 ]; then
+            evtest --query /dev/input/event0 EV_KEY 28
+            if [ $? = 10 ]; then
+                if [ $BOTH_PRESSED_TIME -eq 0 ]; then
+                    BOTH_PRESSED_TIME=$(date +%s)
+                else
+                    CUR_TIME=$(date +%s)
+                    ELAPSED=$((CUR_TIME - BOTH_PRESSED_TIME))
 
-        # Check for reboot combo (SELECT + START)
-        if [ "$SELECT_PRESSED" = "1" ] && [ "$START_PRESSED" = "1" ]; then
-            if [ $BOTH_PRESSED_TIME -eq 0 ]; then
-                BOTH_PRESSED_TIME=$(date +%s)
-            else
-                CUR_TIME=$(date +%s)
-                ELAPSED=$((CUR_TIME - BOTH_PRESSED_TIME))
-                
-                if [ $ELAPSED -ge $MAX_PRESS_LENGTH ]; then
-                    log_message "Key combo held for ${MAX_PRESS_LENGTH} seconds. Rebooting..." "/mnt/SDCARD/System/log/killswitch.log"
-                    for PROC in $PROCESSES; do
-                        if [ "$PROC" = "ra32.trimui" ] || [ "$PROC" = "ra32.trimui_sdl" ]; then
-                            killall "$PROC"
-                        else
-                            killall -9 "$PROC"
-                        fi
-                    done
-                    sleep 0.5
-                    cat /dev/zero > /dev/fb0
-                    sync
-                    reboot
-                    exit 0
+                    if [ $ELAPSED -ge $MAX_PRESS_LENGTH ]; then
+                        log_message "Key combo held for 5 seconds. Rebooting..." "/mnt/SDCARD/System/log/killswitch.log"
+                        for PROC in $PROCESSES; do
+                            if [ "$PROC" = "ra32.trimui" ] || [ "$PROC" = "ra32.trimui_sdl" ]; then # will create a savestate when terminated
+                                killall "$PROC"
+                            else
+                                killall -9 "$PROC"
+                            fi
+                        done
+                        sleep 0.5
+                        cat /dev/zero > /dev/fb0
+                        sync
+                        reboot
+                        exit 0
+                    fi
                 fi
+            else
+                BOTH_PRESSED_TIME=0
             fi
+        else
+            BOTH_PRESSED_TIME=0
         fi
-
-        # Check for screenshot combo (SELECT + MENU + R)
-        if [ "$SELECT_PRESSED" = "1" ] && [ "$MENU_R_PRESSED" = "1" ]; then
-            echo default-on > /sys/devices/platform/sunxi-led/leds/led2/trigger
-            fbscreenshot
-            echo none > /sys/devices/platform/sunxi-led/leds/led2/trigger
-        fi
+        sleep 0.2
     done
 }
 
-hotkey_listener &
+screenshot_hotkey() {
+    while true; do
+        evtest --query /dev/input/event0 EV_KEY 97 # SELECT
+        if [ $? = 10 ]; then
+            evtest --query /dev/input/event0 EV_KEY 109 # MENU + R
+            if [ $? = 10 ]; then
+                echo default-on > /sys/devices/platform/sunxi-led/leds/led2/trigger
+                fbscreenshot
+                echo none > /sys/devices/platform/sunxi-led/leds/led2/trigger
+            fi
+        fi
+        sleep 0.2
+    done
+}
+
+reboot_hotkey &
+screenshot_hotkey &
