5324b838324a23fff70786eac29e70ad36757595
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-22 14:54:45 +0100

    implement USB-C headphone volume control needs testing, thanks to pritchardz7368 from RH discord for pointing this out

---
+++ System/starts/3-sysjson-listeners.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Listens for changes to system.json
+
+. /mnt/SDCARD/System/scripts/helpers.sh
+
+SYSTEM_JSON="/mnt/UDISK/system.json"
+THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "$SYSTEM_JSON" | sed 's:/*$:/:')"
+VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
+
+# Reboot on theme change to fully load theme
+while true; do
+    inotifywait -e modify "$SYSTEM_JSON"
+
+    NEW_THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "$SYSTEM_JSON" | sed 's:/*$:/:')"
+
+    if [ "$NEW_THEME_PATH" != "$THEME_PATH" ]; then
+        killall -9 MainUI
+        cat /dev/zero > /dev/fb0
+        reboot
+    fi
+done &
+
+# USB-C headphone volume control
+while true; do
+    inotifywait -e modify "$SYSTEM_JSON"
+
+    NEW_VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
+
+    if [ "$NEW_VOLUME" != "$VOLUME" ]; then
+        VOLUME="$NEW_VOLUME"
+        amixer sset 'headphone volume' $(($VOLUME * 5))% 
+    fi
+done &
