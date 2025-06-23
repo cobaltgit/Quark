95d5ca3a34fc4afa42d5b2a6e3c3010b2e81ae03
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-23 15:42:27 +0100

    crack the USB-C audio volume control nut for real this time Except for some reason retroarch and ffplay hang on exit until you unplug them

--- System/starts/3-sysjson-listeners.sh
+++ System/starts/3-sysjson-listeners.sh
@@ -7,8 +7,6 @@ SYSTEM_JSON="/mnt/UDISK/system.json"
 THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "$SYSTEM_JSON" | sed 's:/*$:/:')"
 VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
 
-amixer sset 'headphone volume' $(($VOLUME * 5))% &
-
 # Reboot on theme change to fully load theme
 while true; do
     inotifywait -e modify "$SYSTEM_JSON"
@@ -28,8 +26,17 @@ while true; do
 
     NEW_VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
 
+    # only change volume if sound card exists (when USB-C audio output is plugged in)
     if [ "$NEW_VOLUME" != "$VOLUME" ]; then
         VOLUME="$NEW_VOLUME"
-        amixer sset 'headphone volume' $(($VOLUME * 5))% 
+        [ -c "/dev/audio1" ] && nice -2 amixer -c 1 sset 'PCM' $(($VOLUME * 5))% 
+    fi
+done &
+
+# listen for headphones being plugged in, then set volume
+while true; do
+    inotifywait -e create /dev --include 'audio1'
+    if [ -c "/dev/audio1" ]; then
+        nice -2 amixer -c 1 sset 'PCM' $(($VOLUME * 5))%
     fi
 done &
