bdc95ea2a8ea01ea2329cf3330ece6cbe1023d51
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-24 12:38:32 +0100

    guestmode: separate recents, favourites in both MainUI and RetroArch

--- System/starts/8-guestmode.sh
+++ System/starts/8-guestmode.sh
@@ -7,7 +7,22 @@ GUESTMODE_APP="/mnt/SDCARD/Apps/GuestMode"
 
 if [ $(get_setting "user" "guest") = "true" ]; then
     mount -o bind /mnt/SDCARD/Saves/.guest /mnt/SDCARD/Saves
+    mount -o bind /mnt/SDCARD/Roms/favourite2.guest.json /mnt/SDCARD/Roms/favourite2.json
+    mount -o bind /mnt/SDCARD/Roms/recentlist.guest.json /mnt/SDCARD/Roms/recentlist.json
+    mount -o bind /mnt/SDCARD/RetroArch/content_history_guest.lpl /mnt/SDCARD/RetroArch/content_history.lpl
+    mount -o bind /mnt/SDCARD/RetroArch/content_image_history_guest.lpl /mnt/SDCARD/RetroArch/content_image_history.lpl
+    mount -o bind /mnt/SDCARD/RetroArch/content_music_history_guest.lpl /mnt/SDCARD/RetroArch/content_music_history.lpl
+    mount -o bind /mnt/SDCARD/RetroArch/content_favorites_guest.lpl /mnt/SDCARD/RetroArch/content_favorites.lpl
+
     sed -i -e 's|\[OFF\]|\[ON\]|' -e 's|icon-off.png|icon-on.png|' "$GUESTMODE_APP/config.json"
 else
+    umount /mnt/SDCARD/Saves
+    umount /mnt/SDCARD/Roms/favourite2.json
+    umount /mnt/SDCARD/Roms/recentlist.json
+    umount /mnt/SDCARD/RetroArch/content_history.lpl
+    umount /mnt/SDCARD/RetroArch/content_image_history.lpl
+    umount /mnt/SDCARD/RetroArch/content_music_history.lpl
+    umount /mnt/SDCARD/RetroArch/content_favorites.lpl
+
     sed -i -e 's|\[ON\]|\[OFF\]|' -e 's|icon-on.png|icon-off.png|' "$GUESTMODE_APP/config.json"
 fi
