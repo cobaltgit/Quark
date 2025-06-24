553c855243fb47b1d610951e14d30505795cd11a
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-24 10:44:26 +0100

    guestmode: fix boot script resolving wrong dir....again

--- System/starts/8-guestmode.sh
+++ System/starts/8-guestmode.sh
@@ -7,7 +7,7 @@ GUESTMODE_APP="/mnt/SDCARD/Apps/GuestMode"
 
 if [ $(get_setting "user" "guest") = "true" ]; then
     mount -o bind /mnt/SDCARD/Saves/.guest /mnt/SDCARD/Saves
-    sed -i -e 's|\[OFF\]|\[ON\]|' -e 's|icon-off.png|icon-on.png|' "$(dirname "$0")/config.json"
+    sed -i -e 's|\[OFF\]|\[ON\]|' -e 's|icon-off.png|icon-on.png|' "$GUESTMODE_APP/config.json"
 else
-    sed -i -e 's|\[ON\]|\[OFF\]|' -e 's|icon-on.png|icon-off.png|' "$(dirname "$0")/config.json"
+    sed -i -e 's|\[ON\]|\[OFF\]|' -e 's|icon-on.png|icon-off.png|' "$GUESTMODE_APP/config.json"
 fi
