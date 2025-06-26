e1aaa883a4462c0ca687fe4d5823ab0598771b07
cobaltgit <cobaltsecuremail312@proton.me>
2025-06-26 13:22:44 +0100

    combine inotifywait system.json listeners

--- System/starts/3-sysjson-listeners.sh
+++ System/starts/3-sysjson-listeners.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Listens for changes to system.json
+# Listens for system changes
 
 . /mnt/SDCARD/System/scripts/helpers.sh
 
@@ -7,26 +7,20 @@ SYSTEM_JSON="/mnt/UDISK/system.json"
 THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "$SYSTEM_JSON" | sed 's:/*$:/:')"
 VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
 
-# Reboot on theme change to fully load theme
+# Combined theme change and volume control listener
 while true; do
     inotifywait -e modify "$SYSTEM_JSON"
 
+    # Check for theme changes
     NEW_THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "$SYSTEM_JSON" | sed 's:/*$:/:')"
-
     if [ "$NEW_THEME_PATH" != "$THEME_PATH" ]; then
         killall -9 MainUI
         cat /dev/zero > /dev/fb0
         reboot
     fi
-done &
 
-# USB-C headphone volume control
-while true; do
-    inotifywait -e modify "$SYSTEM_JSON"
-
+    # Check for volume changes
     NEW_VOLUME="$(awk '/\"vol\":/ { gsub(/[,]/,"",$2); print $2}' "$SYSTEM_JSON")"
-
-    # only change volume if sound card exists (when USB-C audio output is plugged in)
     if [ "$NEW_VOLUME" != "$VOLUME" ]; then
         VOLUME="$NEW_VOLUME"
         [ -c "/dev/audio1" ] && nice -2 amixer -c 1 sset 'PCM' $(($VOLUME * 5))% 
