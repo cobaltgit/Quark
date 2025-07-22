c24806045fde7771e40d1387a374426497c66dd9
cobaltgit <cobaltsecuremail312@proton.me>
2025-07-22 12:42:00 +0100

    EmuClean: remove _cache7.db files for systems with ROMs present Will refresh the MainUI gamelist

--- Apps/EmuClean/launch.sh
+++ Apps/EmuClean/launch.sh
@@ -15,17 +15,19 @@ display -t "Refreshing displayed emulators..."
 
 for EMU in "$EMU_DIR"/*; do
     if [ -d "$EMU" ]; then
+        BASENAME="$(basename "$EMU")"
         EXTENSIONS="$(awk -F ':' '/extlist/ {print $2}' "$EMU/config.json" | sed 's/^[[:space:]]*//; s/[",]//g')" # no jq? no problem. might add it later down the line though
         ROM_DIR="$EMU/$(awk -F ':' '/rompath/ {print $2}' "$EMU/config.json" | sed 's/^[[:space:]]*//; s/[",]//g')"
         ROM_COUNT="$(find "$ROM_DIR" -type f ! -path "*/Imgs/*" ! -name *.xml ! -name *.txt ! -name ".gitkeep" ! -name "*cache7.db" | sed '/^\s*$/d' | grep -icE "\.($EXTENSIONS)$")"
         
         if [ "$ROM_COUNT" -eq 0 ]; then
             log_message "EmuClean: no roms found in $EMU, hiding" "$LOG_FILE"
-            sed -i 's/^{*$/{{/' "$EMU/config.json" # break config.json so emulator doesn't show
+            sed -i 's/^{*$/{{/' "$EMU/config.json" # intentionally break config.json so system doesn't show in MainUI
             HIDDEN_COUNT=$((HIDDEN_COUNT + 1))
         elif [ "$ROM_COUNT" -gt 0 ]; then
             log_message "EmuClean: $ROM_COUNT roms detected in system $EMU, showing" "$LOG_FILE"
-            sed -i 's/^{{*$/{/' "$EMU/config.json" # fix config.json to show emulator
+            sed -i 's/^{{*$/{/' "$EMU/config.json" # fix config.json to show system in MainUI
+            rm -f "/mnt/SDCARD/Roms/$BASENAME/${BASENAME}_cache7.db"
             SHOWN_COUNT=$((SHOWN_COUNT + 1))
         fi
     fi
