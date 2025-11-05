e0608145f4f45c83229f029ecc740f144ec8a0ac
cobaltgit <cobaltsecuremail312@proton.me>
2025-11-05 16:37:43 +0000

    bootsound: don't play when loading a quicksave

--- System/starts/8-boot-sound.sh
+++ System/starts/8-boot-sound.sh
@@ -2,5 +2,7 @@
 
 THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "/mnt/UDISK/system.json" | sed 's:/*$:/:')"
 BOOT_SOUND="$THEME_PATH/sound/boot.wav"
+QUICKSAVE_CMD_TO_RUN="/mnt/SDCARD/Saves/.quicksave.sh"
 
-[ -f "$BOOT_SOUND" ] && aplay "$BOOT_SOUND" &
+[ -f "$BOOT_SOUND" ] && ! [ -f "$QUICKSAVE_CMD_TO_RUN" ] && \
+	aplay "$BOOT_SOUND" &
