0de6bd62e14f7e918a402af94b19a2f45a70f644
cobaltgit <cobaltsecuremail312@proton.me>
2026-01-26 11:18:40 +0000

    bootsound: correct quicksave cmd_to_run

--- System/starts/6-boot-sound.sh
+++ System/starts/6-boot-sound.sh
@@ -2,7 +2,7 @@
 
 THEME_PATH="$(awk -F'"' '/"theme":/ {print $4}' "/mnt/UDISK/system.json" | sed 's:/*$:/:')"
 BOOT_SOUND="$THEME_PATH/sound/boot.wav"
-QUICKSAVE_CMD_TO_RUN="/mnt/SDCARD/Saves/.quicksave.sh"
+QUICKSAVE_CMD_TO_RUN="/mnt/SDCARD/Saves/quicksave_cmd_to_run.sh"
 
 if ! [ -f "/tmp/.play_bootvideo" ]; then
     [ -f "$BOOT_SOUND" ] && ! [ -f "$QUICKSAVE_CMD_TO_RUN" ] && \
