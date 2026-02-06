b72e2897e50fcd7abb97982dab88eb8bc4c1893e
cobaltgit <cobaltsecuremail312@proton.me>
2026-02-06 12:47:17 +0000

    fbscreenshot: compute colour conversion at compile time

--- src/common/fbscreenshot.nim
+++ src/common/fbscreenshot.nim
@@ -5,6 +5,19 @@ const
   FbHeight = 320
   FbPixels = FbWidth * FbHeight
 
+  # RGB565 -> RGB888 lookup tables computed at compile time
+  FiveToEight: array[32, uint8] = block:
+    var arr: array[32, uint8]
+    for i in 0..31:
+      arr[i] = uint8((i shl 3) or (i shr 2))
+    arr
+  
+  SixToEight: array[64, uint8] = block:
+    var arr: array[64, uint8]
+    for i in 0..63:
+      arr[i] = uint8((i shl 2) or (i shr 4))
+    arr
+
 proc fbscreenshot*(output: string) =
   let fbFile = open("/dev/fb0", fmRead)
   defer: fbFile.close()
@@ -15,24 +28,23 @@ proc fbscreenshot*(output: string) =
   if bytesRead != fbData.len:
     raise newException(IOError, "Failed to read complete framebuffer data")
   
-  var rotatedPixels = newSeq[uint8](FbHeight * FbWidth * 4)
+  var rotatedPixels = newSeqUninit[uint8](FbHeight * FbWidth * 4)
 
+  var srcIdx = 0
   for y in 0..<FbHeight:
     for x in 0..<FbWidth:
-      let idx = (y * FbWidth * 2 + x * 2)
-      let pixel = uint16(fbData[idx]) or (uint16(fbData[idx + 1]) shl 8)
+      let pixel = uint16(fbData[srcIdx]) or (uint16(fbData[srcIdx + 1]) shl 8)
+      srcIdx += 2
 
-      let r = uint8((pixel shr 11) and 0x1F)
-      let g = uint8((pixel shr 5) and 0x3F)
-      let b = uint8(pixel and 0x1F)
+      let r = (pixel shr 11) and 0x1F
+      let g = (pixel shr 5) and 0x3F
+      let b = pixel and 0x1F
 
-      let dstX = FbHeight - 1 - y
-      let dstY = x
-      let dstIdx = (x * FbHeight + dstX) * 4
+      let dstIdx = (x * FbHeight + (FbHeight - 1 - y)) * 4
 
-      rotatedPixels[dstIdx] = (r shl 3) or (r shr 2)
-      rotatedPixels[dstIdx + 1] = (g shl 2) or (g shr 4)
-      rotatedPixels[dstIdx + 2] = (b shl 3) or (b shr 2)
+      rotatedPixels[dstIdx] = FiveToEight[r]
+      rotatedPixels[dstIdx + 1] = SixToEight[g]
+      rotatedPixels[dstIdx + 2] = FiveToEight[b]
       rotatedPixels[dstIdx + 3] = 255
   
   discard savePNG32(output, rotatedPixels, FbHeight, FbWidth)
